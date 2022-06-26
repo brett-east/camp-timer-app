f2baf175b3a902c9e3483dbcd842ac121f05ae7c
Brett East <brett@playdraft.com>
2022-06-26 13:17:59 -0400

    add manual sound player

--- src/components/play-sound-cell/index.tsx
+++ src/components/play-sound-cell/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './styles.module.scss';
 
@@ -7,13 +7,67 @@ interface PlaySoundCellProps {
 }
 
 const PlaySoundCell = (props: PlaySoundCellProps) => {
+  const SOUND_PATH = '/sounds';
+  const [selectedSound, setSelectedSound] = useState(`${process.env.PUBLIC_URL}${SOUND_PATH}/first_call.mp3`);
+  const [playing, setPlaying] = useState(false);
   return (
     <div className={styles.playSoundCell}>
-      <p>
-        play sound PlaySoundCell
-      </p>
+      <select
+        className={styles.dropDown}
+        onChange={(e) => (setSelectedSound(`${process.env.PUBLIC_URL}${SOUND_PATH}/${e.target.value}`))}
+      >
+        {SOUNDS.map(sound => (
+          <option
+            key={sound.id}
+            value={sound.track}
+          >
+            {sound.title}
+          </option>
+        ))}
+      </select>
+      <audio
+        className={styles.audioPlayer}
+        controls
+        src={selectedSound}
+      >
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
     </div>
   );
 };
 
 export default PlaySoundCell;
+
+const SOUNDS = [
+  {
+    id: 'firstCall',
+    title: 'First call',
+    track: 'first_call.mp3',
+  },
+  {
+    id: 'flagDown',
+    title: 'Flag down',
+    track: 'flag_down.mp3',
+  },
+  {
+    id: 'flagUp',
+    title: 'Flag up',
+    track: 'flag_up.mp3',
+  },
+  {
+    id: 'reveille',
+    title: 'Reveille',
+    track: 'reveille.mp3',
+  },
+  {
+    id: 'sticks',
+    title: 'Sticks',
+    track: 'sticks.mp3',
+  },
+  {
+    id: 'taps',
+    title: 'Taps',
+    track: 'taps.mp3',
+  },
+];
